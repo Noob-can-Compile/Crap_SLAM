d83fa60ac316dba552b2882c1981a10ebd277288
Noob_can_Compile <krunalkshirsagar29@gmail.com>
2020-03-22 23:13:30 +0530

    Performed Feature detection extraction and matching with better code

--- slam.py
+++ slam.py
@@ -1,26 +1,42 @@
 import time
 import cv2
+import numpy as np
 from display import Display
 
-
-width = 1280//2 
-height = 720//2
+width = 1280//2  #1920//2
+height = 720//2  #1080//2   
 
 disp = Display(width, height)
-orb = cv2.ORB_create()
 
+class FeatureExtractor(object):
+    def __init__(self):
+        self.orb = cv2.ORB_create(100)
+        self.bf = cv2.BFMatcher()
+        self.last = None
+
+    def extract(self, img):
+        fts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8),3000, qualityLevel = 0.01, minDistance = 1)
+        kps = [cv2.KeyPoint(x = f[0][0], y = f[0][1], _size = 20) for f in fts]
+        kps, des = self.orb.compute(img, kps)
+
+        if self.last is not None:
+            matches = self.bf.match(des, self.last['des'])
+            print(matches)
+
+        self.last = {'kps' : kps, 'des' : des}
+        return kps, des
+
+fe = FeatureExtractor()
 
 def frames_per_motion(img):
     img = cv2.resize(img, (width, height))
-    kp, des = orb.detectAndCompute(img,None)
-    for keypoint in kp:
-        u,v = map(lambda x: int(round(x)), keypoint.pt)
-        cv2.circle(img, (u,v), color = (0,255,0), radius = 3, thickness = 2)
+    kp, des = fe.extract(img)
+
+    for point in kp:
+        u,v = map(lambda x: int(round(x)), point.pt)
+        cv2.circle(img, (u,v), color = (0,255,0), radius = 1, thickness = 2)
+    
     disp.paint(img)
-    #cv2.imshow("image", img)
-    #cv2.waitKey(1)
-    #print(img.shape)
-    #print(img)
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("car.mp4")
