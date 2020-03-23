895b435ee4440d5d1a6db2adc784a5caa1967d7f
Noob_can_Compile <krunalkshirsagar29@gmail.com>
2020-03-23 18:52:11 +0530

    Added seperate feature extractor class file

--- slam.py
+++ slam.py
@@ -2,35 +2,19 @@ import time
 import cv2
 import numpy as np
 from display import Display
+from extractor import Extractor
 
 width = 1280//2  #1920//2
 height = 720//2  #1080//2   
 
 disp = Display(width, height)
-
-class FeatureExtractor(object):
-    def __init__(self):
-        self.orb = cv2.ORB_create(100)
-        self.bf = cv2.BFMatcher()
-        self.last = None
-
-    def extract(self, img):
-        fts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8),3000, qualityLevel = 0.01, minDistance = 1)
-        kps = [cv2.KeyPoint(x = f[0][0], y = f[0][1], _size = 20) for f in fts]
-        kps, des = self.orb.compute(img, kps)
-
-        if self.last is not None:
-            matches = self.bf.match(des, self.last['des'])
-            print(matches)
-
-        self.last = {'kps' : kps, 'des' : des}
-        return kps, des
-
-fe = FeatureExtractor()
+fe = Extractor()
 
 def frames_per_motion(img):
     img = cv2.resize(img, (width, height))
-    kp, des = fe.extract(img)
+    kp, des, matches = fe.extract(img)
+    if matches is None:
+        return
 
     for point in kp:
         u,v = map(lambda x: int(round(x)), point.pt)
